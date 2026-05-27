5755e5b87707ef7684486c72cf1b89ddb43e0649
KigoJomo <kigojomo@gmail.com>
2026-05-27 23:10:17 +0300

    feat: add webview element JSX type declarations

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,24 @@
 /// <reference types="vite/client" />
+
+import type * as React from 'react'
+
+declare global {
+	namespace JSX {
+		interface IntrinsicElements {
+			webview: React.DetailedHTMLProps<
+				React.HTMLAttributes<HTMLElement> & {
+					src?: string
+					partition?: string
+					allowpopups?: boolean
+					autosize?: boolean
+					minheight?: string
+					minwidth?: string
+					webpreferences?: string
+				},
+				HTMLElement
+			>
+		}
+	}
+}
+
+export {}
